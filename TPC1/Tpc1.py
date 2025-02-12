96b38c1f0afe3e637123430c0cad598b4be0c75a
MarcoGoncalves123 <147045046+MarcoGoncalves123@users.noreply.github.com>
2025-02-12 09:16:14 +0000

    Fix digitos

--- TPC1/Tpc1.py
+++ TPC1/Tpc1.py
@@ -4,24 +4,23 @@ def On_Off(linha):
     i = 0
     res = 0
     on = True
+    acc = ''
     while i < len(linha):
-        if(linha[i] == "="):
-            print(res)
-            i += 1 
-        elif linha[i] in "1234567890" and on:
-            res += int(linha[i])
-            i += 1
-        elif linha[i].lower() == "o":
-            i += 1
-            if(linha[i].lower() == "n"):
-                i += 1
+        if (linha[i] in "1234567890" and on):
+            acc += linha[i]
+        elif acc and on:
+            res += int(acc)
+            acc = '' 
+                        
+        if(linha[i] == '='):
+            print(res) 
+            
+        if linha[i].lower() == "o":
+            if(linha[i+1].lower() == "n"):
                 on = True
-            elif(linha[i].lower() == "f"):
-                i += 1
-                if(linha[i].lower() == "f"):
+            elif(linha[i+1].lower() == "f"):
+                if(linha[i+2].lower() == "f"):
                     on = False
-                    i += 1
-        else:
-            i += 1
+        i+=1
     
-On_Off("12abc34=On56xyz789=OFF987=On0abc123=")
+On_Off("12abc34=On56xyz789=OFF987=Onabc123=")
